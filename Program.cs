099121186234736eab0a3484c3b180407ae20c9b
Asmaa-elmekawy <120722726+Asmaa-elmekawy@users.noreply.github.com>
2022-12-16 16:24:55 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Data;
+
+namespace project_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello at Prime application");
+
+            Console.WriteLine("please enter the 1st Number");
+            int FirstNumber=int.Parse(Console.ReadLine());
+
+            Console.WriteLine("please enter 2nd Number");
+            int SecondNumber=int.Parse(Console.ReadLine());
+
+            int i;
+
+            for (i =FirstNumber; i<=SecondNumber; i++)
+            {
+                if(i==2)
+                {
+                    Console.WriteLine(i+"is prime");
+                }
+                else if(i==3)
+                {
+                    Console.WriteLine(i+"is prime");
+                }
+                else if(i==5)
+                {
+                    Console.WriteLine(i + "is prime");
+                }
+            }
+            for (i = FirstNumber; i <= SecondNumber; i++)
+            {
+                if (i == 1)
+                {
+                    continue;
+                }
+
+                else if (i % 2 == 0)
+                {
+                    continue;
+                }
+                else if (i % 3 == 0)
+                {
+                    continue;
+                }
+                else if (i % 5 == 0)
+                {
+                    continue;
+                }
+                Console.WriteLine(i + "is prime");
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
